b7180e6cc843ba3740d7e7af8b366550b57b0bff
Maira Bello <maira.araujo@liferay.com>
2014-09-18 09:32:37 +0200

    Source formatting

--- test/fixture/sandbox.js
+++ test/fixture/sandbox.js
@@ -3,6 +3,6 @@
 var nodeunit = require('nodeunit');
 
 if (typeof exports === 'object') {
-  var context = nodeunit.utils.sandbox(['./build/lfr.js'], {});
+  var context = nodeunit.utils.sandbox(['./build/lfr.js'], {console: console});
   module.exports = context.lfr;
 }

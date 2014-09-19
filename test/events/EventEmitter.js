80aa7fcf4e0f246df3d6ef57b59d363f24134068
Maira Bello <maira.araujo@liferay.com>
2014-09-19 17:48:23 +0200

    Allows listening to multiple events on same addEventListener call - Fixes #15

--- test/events/EventEmitter.js
+++ test/events/EventEmitter.js
@@ -88,6 +88,30 @@ describe('EventEmitter', function() {
     assert.strictEqual(3, listener2.callCount);
   });
 
+  it('should listen to multiple events on the same call', function() {
+    var listener = sinon.stub();
+
+    this.emitter.on(['namespaced.event', 'event'], listener);
+
+    this.emitter.emit('namespaced.event');
+    assert.strictEqual(1, listener.callCount);
+
+    this.emitter.emit('event');
+    assert.strictEqual(2, listener.callCount);
+  });
+
+  it('should listen to events through `addListener`', function() {
+    var listener = sinon.stub();
+
+    this.emitter.addListener(['namespaced.event', 'event'], listener);
+
+    this.emitter.emit('namespaced.event');
+    assert.strictEqual(1, listener.callCount);
+
+    this.emitter.emit('event');
+    assert.strictEqual(2, listener.callCount);
+  });
+
   it('should listen to event a single time through `once`', function() {
     var listener = sinon.stub();
 
@@ -99,6 +123,25 @@ describe('EventEmitter', function() {
 
     this.emitter.emit('event');
     assert.strictEqual(1, listener.callCount);
+  });
+
+  it('should listen to multiple events on the same call to `once`', function() {
+    var listener = sinon.stub();
+
+    this.emitter.once(['event', 'event2'], listener);
+    assert.strictEqual(0, listener.callCount);
+
+    this.emitter.emit('event');
+    assert.strictEqual(1, listener.callCount);
+
+    this.emitter.emit('event');
+    assert.strictEqual(1, listener.callCount);
+
+    this.emitter.emit('event2');
+    assert.strictEqual(2, listener.callCount);
+
+    this.emitter.emit('event2');
+    assert.strictEqual(2, listener.callCount);
   });
 
   it('should listen to event a fixed number of times through `many`', function() {
@@ -115,6 +158,30 @@ describe('EventEmitter', function() {
 
     this.emitter.emit('event');
     assert.strictEqual(2, listener.callCount);
+  });
+
+  it('should listen to multiple events on the same call to `many`', function() {
+    var listener = sinon.stub();
+
+    this.emitter.many(['event', 'event2'], 2, listener);
+
+    this.emitter.emit('event');
+    assert.strictEqual(1, listener.callCount);
+
+    this.emitter.emit('event');
+    assert.strictEqual(2, listener.callCount);
+
+    this.emitter.emit('event');
+    assert.strictEqual(2, listener.callCount);
+
+    this.emitter.emit('event2');
+    assert.strictEqual(3, listener.callCount);
+
+    this.emitter.emit('event2');
+    assert.strictEqual(4, listener.callCount);
+
+    this.emitter.emit('event2');
+    assert.strictEqual(4, listener.callCount);
   });
 
   it('should ignore calls to `many` with non positive number', function() {
@@ -142,6 +209,21 @@ describe('EventEmitter', function() {
     assert.strictEqual(0, listener2.callCount);
   });
 
+  it('should detach listeners subscribed multiple times to same event', function() {
+    var listener = sinon.stub();
+    var listener2 = sinon.stub();
+
+    this.emitter.on('event', listener);
+    this.emitter.on('event', listener);
+    this.emitter.on('event', listener);
+    this.emitter.on('event', listener2);
+
+    this.emitter.off('event', listener);
+    this.emitter.emit('event');
+    assert.strictEqual(0, listener.callCount);
+    assert.strictEqual(1, listener2.callCount);
+  });
+
   it('should detach events via return value', function() {
     var listener = sinon.stub();
     var listener2 = sinon.stub();
@@ -152,6 +234,58 @@ describe('EventEmitter', function() {
     handle.removeListener();
     this.emitter.emit('event');
     assert.strictEqual(1, listener.callCount);
+    assert.strictEqual(0, listener2.callCount);
+  });
+
+  it('should detach events that were listened together', function() {
+    var listener = sinon.stub();
+    var listener2 = sinon.stub();
+
+    this.emitter.on(['event', 'event2'], listener);
+    this.emitter.on(['event', 'event2'], listener2);
+    this.emitter.off(['event', 'event2'], listener2);
+
+    this.emitter.emit('event');
+    assert.strictEqual(1, listener.callCount);
+    assert.strictEqual(0, listener2.callCount);
+
+    this.emitter.emit('event2');
+    assert.strictEqual(2, listener.callCount);
+    assert.strictEqual(0, listener2.callCount);
+  });
+
+  it('should detach multiple events with the same call', function() {
+    var listener = sinon.stub();
+    var listener2 = sinon.stub();
+
+    this.emitter.on('event', listener);
+    this.emitter.on('event2', listener);
+    this.emitter.on('event2', listener2);
+    this.emitter.off(['event', 'event2'], listener);
+
+    this.emitter.emit('event');
+    assert.strictEqual(0, listener.callCount);
+    assert.strictEqual(0, listener2.callCount);
+
+    this.emitter.emit('event2');
+    assert.strictEqual(0, listener.callCount);
+    assert.strictEqual(1, listener2.callCount);
+  });
+
+  it('should detach events that were listened together via return value', function() {
+    var listener = sinon.stub();
+    var listener2 = sinon.stub();
+
+    this.emitter.on(['event', 'event2'], listener);
+    var handle = this.emitter.on(['event', 'event2'], listener2);
+    handle.removeListener();
+
+    this.emitter.emit('event');
+    assert.strictEqual(1, listener.callCount);
+    assert.strictEqual(0, listener2.callCount);
+
+    this.emitter.emit('event2');
+    assert.strictEqual(2, listener.callCount);
     assert.strictEqual(0, listener2.callCount);
   });
 
@@ -185,6 +319,28 @@ describe('EventEmitter', function() {
     assert.strictEqual(0, listener.callCount);
   });
 
+  it('should detach events listened together through `many`', function() {
+    var listener = sinon.stub();
+
+    this.emitter.many(['event', 'event2'], 2, listener);
+    this.emitter.off(['event', 'event2'], listener);
+
+    this.emitter.emit('event');
+    this.emitter.emit('event2');
+    assert.strictEqual(0, listener.callCount);
+  });
+
+  it('should detach events listened together through `many` via return value', function() {
+    var listener = sinon.stub();
+
+    var handle = this.emitter.many(['event', 'event2'], 2, listener);
+    handle.removeListener();
+
+    this.emitter.emit('event');
+    this.emitter.emit('event2');
+    assert.strictEqual(0, listener.callCount);
+  });
+
   it('should detach events listened through `many` via return value', function() {
     var listener = sinon.stub();
 
@@ -193,6 +349,19 @@ describe('EventEmitter', function() {
     this.emitter.emit('event');
 
     assert.strictEqual(0, listener.callCount);
+  });
+
+  it('should detach events through `removeListener`', function() {
+    var listener = sinon.stub();
+    var listener2 = sinon.stub();
+
+    this.emitter.on('event', listener);
+    this.emitter.on('event', listener2);
+
+    this.emitter.removeListener('event', listener2);
+    this.emitter.emit('event');
+    assert.strictEqual(1, listener.callCount);
+    assert.strictEqual(0, listener2.callCount);
   });
 
   it('should remove all listeners', function() {
@@ -225,6 +394,26 @@ describe('EventEmitter', function() {
     assert.strictEqual(1, listener2.callCount);
   });
 
+  it('should remove all listeners of the given types', function() {
+    var listener1 = sinon.stub();
+    var listener2 = sinon.stub();
+    var listener3 = sinon.stub();
+
+    this.emitter.on('event1', listener1);
+    this.emitter.on('event2', listener2);
+    this.emitter.on('event3', listener3);
+
+    this.emitter.removeAllListeners(['event1', 'event2']);
+    this.emitter.emit('event1');
+    assert.strictEqual(0, listener1.callCount);
+
+    this.emitter.emit('event2');
+    assert.strictEqual(0, listener2.callCount);
+
+    this.emitter.emit('event3');
+    assert.strictEqual(1, listener3.callCount);
+  });
+
   it('should warn when max number of listeners is reached', function() {
     var originalWarningFn = console.warn;
     console.warn = sinon.stub();
@@ -235,13 +424,12 @@ describe('EventEmitter', function() {
     this.emitter.on('event1', sinon.stub());
     assert.strictEqual(0, console.warn.callCount, 'Should not warn before max');
 
-    this.emitter.on('event', sinon.stub());
+    this.emitter.on(['event', 'event1'], sinon.stub());
     assert.strictEqual(1, console.warn.callCount, 'Max listeners reached for event');
 
     this.emitter.on('event', sinon.stub());
     assert.strictEqual(1, console.warn.callCount, 'Should not warn twice for same type');
 
-    this.emitter.on('event1', sinon.stub());
     this.emitter.on('event1', sinon.stub());
     assert.strictEqual(2, console.warn.callCount, 'Max listeners reached for event1');
 

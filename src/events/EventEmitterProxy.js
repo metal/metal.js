20ec4c172a5fde20f6120bcc550f252d37321c52
Maira Bello <maira.araujo@liferay.com>
2015-01-09 14:24:51 -0300

    Adds EventEmitterProxy

---
+++ src/events/EventEmitterProxy.js
@@ -0,0 +1,84 @@
+(function() {
+  'use strict';
+
+  /**
+   * EventEmitterProxy utility. It's responsible for linking two EventEmitter
+   * instances together, emitting all events from the first emitter through
+   * the second one.
+   * @param {lfr.EventEmitter} originEmitter
+   * @param {lfr.EventEmitter} targetEmitter
+   * @constructor
+   */
+  lfr.EventEmitterProxy = function(originEmitter, targetEmitter) {
+    this.originEmitter_ = originEmitter;
+    this.targetEmitter_ = targetEmitter;
+    this.proxiedEvents_ = {};
+
+    this.startProxy_();
+  };
+  lfr.inherits(lfr.EventEmitterProxy, lfr.Disposable);
+
+  /**
+   * The origin emitter. This emitter's events will be proxied through the
+   * target emitter.
+   * @type {lfr.EventEmitter}
+   * @default null
+   * @protected
+   */
+  lfr.EventEmitterProxy.prototype.originEmitter_ = null;
+
+  /**
+   * Holds a map of events from the origin emitter that are already being proxied.
+   * @type {Object}
+   * @default null
+   * @protected
+   */
+  lfr.EventEmitterProxy.prototype.proxiedEvents_ = null;
+
+  /**
+   * The target emitter. This emitter will emit all events that come from
+   * the origin emitter.
+   * @type {lfr.EventEmitter}
+   * @default null
+   * @protected
+   */
+  lfr.EventEmitterProxy.prototype.targetEmitter_ = null;
+
+  /**
+   * @inheritDoc
+   */
+  lfr.EventEmitterProxy.prototype.disposeInternal = function() {
+    for (var event in this.proxiedEvents_) {
+      this.originEmitter_.removeListener(event, this.proxiedEvents_[event]);
+    }
+
+    this.proxiedEvents_ = null;
+    this.originEmitter_ = null;
+    this.targetEmitter_ = null;
+  };
+
+  /**
+   * Proxies the given event from the origin to the target emitter.
+   * @param {string} event
+   */
+  lfr.EventEmitterProxy.prototype.proxyEvent_ = function(event) {
+    if (this.proxiedEvents_[event]) {
+      return;
+    }
+
+    var self = this;
+    this.proxiedEvents_[event] = function() {
+      var args = [event].concat(Array.prototype.slice.call(arguments, 0));
+      self.targetEmitter_.emit.apply(self.targetEmitter_, args);
+    };
+    this.originEmitter_.on(event, this.proxiedEvents_[event]);
+  };
+
+  /**
+   * Starts proxying all events from the origin to the target emitter.
+   * @protected
+   */
+  lfr.EventEmitterProxy.prototype.startProxy_ = function() {
+    this.targetEmitter_.on('newListener', lfr.bind(this.proxyEvent_, this));
+  };
+}());
---
+++ test/events/EventEmitterProxy.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var assert = require('assert');
+var sinon = require('sinon');
+require('../fixture/sandbox.js');
+
+describe('EventEmitterProxy', function() {
+  it('should proxy event from origin to target', function() {
+    var emitter1 = new lfr.EventEmitter();
+    var emitter2 = new lfr.EventEmitter();
+    new lfr.EventEmitterProxy(emitter1, emitter2);
+
+    var listener = sinon.stub();
+    emitter2.on('event1', listener);
+    emitter1.emit('event1', 1, 2);
+
+    assert.strictEqual(1, listener.callCount);
+    assert.strictEqual(1, listener.args[0][0]);
+    assert.strictEqual(2, listener.args[0][1]);
+  });
+
+  it('should only emit proxied event once per listener', function() {
+    var emitter1 = new lfr.EventEmitter();
+    var emitter2 = new lfr.EventEmitter();
+    new lfr.EventEmitterProxy(emitter1, emitter2);
+
+    var listener1 = sinon.stub();
+    emitter2.on('event1', listener1);
+    var listener2 = sinon.stub();
+    emitter2.on('event1', listener2);
+    emitter1.emit('event1', 1, 2);
+
+    assert.strictEqual(1, listener1.callCount);
+    assert.strictEqual(1, listener2.callCount);
+  });
+
+  it('should not proxy events after disposed', function() {
+    var emitter1 = new lfr.EventEmitter();
+    var emitter2 = new lfr.EventEmitter();
+    var proxy = new lfr.EventEmitterProxy(emitter1, emitter2);
+
+    var listener = sinon.stub();
+    emitter2.on('event1', listener);
+
+    proxy.dispose();
+    emitter1.emit('event1', 1, 2);
+    assert.strictEqual(0, listener.callCount);
+  });
+});

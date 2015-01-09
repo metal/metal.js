24cde0852fe5b9ff0a0348e1a362be9047b34037
Maira Bello <maira.araujo@liferay.com>
2015-01-09 16:29:20 -0300

    Removes listeners from socket when disposing WebSocketTransport

--- src/net/WebSocketTransport.js
+++ src/net/WebSocketTransport.js
@@ -9,6 +9,7 @@
    */
   lfr.WebSocketTransport = function(uri) {
     lfr.WebSocketTransport.base(this, 'constructor', uri);
+    this.socketEvents_ = [];
   };
   lfr.inherits(lfr.WebSocketTransport, lfr.Transport);
 
@@ -39,6 +40,32 @@
   lfr.WebSocketTransport.prototype.socket = null;
 
   /**
+   * Holds information about the events that are being listened in the socket.
+   * This is necessary for removing these listeners when the transport is
+   * disposed.
+   * @type {Array<Object>}
+   * @default null
+   */
+  lfr.WebSocketTransport.prototype.socketEvents_ = null;
+
+  /**
+   * Listens to the specified event on the socket. This action should always be done
+   * through this method, since it will appropriately hold the necessary information
+   * so the listener can be removed when the transport is disposed.
+   * @param {!Socket.IO} socket
+   * @param {string} event
+   * @param {!function} listener
+   * @protected
+   */
+  lfr.WebSocketTransport.prototype.addSocketListener_ = function(socket, event, listener) {
+    this.socketEvents_.push({
+      event: event,
+      listener: listener
+    });
+    socket.on(event, listener);
+  };
+
+  /**
    * @inheritDoc
    */
   lfr.WebSocketTransport.prototype.close = function() {
@@ -54,16 +81,14 @@
    * @protected
    */
   lfr.WebSocketTransport.prototype.createSocket_ = function() {
-    /*global io*/
-    if (!io) {
-      throw new Error('Socket.IO client not found');
-    }
+    this.verifySocketIOExists_();
+
     var socket = io(this.getUri());
-    socket.on('connect', lfr.bind(this.onSocketConnect_, this));
-    socket.on('disconnect', lfr.bind(this.onSocketDisconnect_, this));
-    socket.on('error', lfr.bind(this.onSocketError_, this));
-    socket.on('data', lfr.bind(this.onSocketData_, this));
-    socket.on('message', lfr.bind(this.onSocketMessage_, this));
+    this.addSocketListener_(socket, 'connect', lfr.bind(this.onSocketConnect_, this));
+    this.addSocketListener_(socket, 'disconnect', lfr.bind(this.onSocketDisconnect_, this));
+    this.addSocketListener_(socket, 'error', lfr.bind(this.onSocketError_, this));
+    this.addSocketListener_(socket, 'data', lfr.bind(this.onSocketData_, this));
+    this.addSocketListener_(socket, 'message', lfr.bind(this.onSocketMessage_, this));
     return socket;
   };
 
@@ -71,12 +96,14 @@
    * @inheritDoc
    */
   lfr.WebSocketTransport.prototype.disposeInternal = function() {
-    var self = this;
     this.once('close', function() {
-      self.proxy_.dispose();
-      self.proxy_ = null;
+      this.proxy_.dispose();
+      this.proxy_ = null;
+
+      this.removeSocketListeners_();
+      this.socket = null;
     });
-    lfr.WebSocketTransport.base(self, 'disposeInternal');
+    lfr.WebSocketTransport.base(this, 'disposeInternal');
   };
 
   /**
@@ -149,11 +176,36 @@
   };
 
   /**
+   * Removes all listeners that were attached to the socket by this transport.
+   * @protected
+   */
+  lfr.WebSocketTransport.prototype.removeSocketListeners_ = function() {
+    for (var i = 0; i < this.socketEvents_.length; i++) {
+      this.socket.removeListener(
+        this.socketEvents_[i].event,
+        this.socketEvents_[i].listener
+      );
+    }
+  };
+
+  /**
    * Sets this transport to be RESTful or not.
    * @param {boolean} restful
    */
   lfr.WebSocketTransport.prototype.setRestful = function(restful) {
     this.restful_ = restful;
+  };
+
+  /**
+   * Verifies that the `io` global function exists, throwing an error otherwise.
+   * @throws {Error}
+   * @protected
+   */
+  lfr.WebSocketTransport.prototype.verifySocketIOExists_ = function() {
+    /*global io*/
+    if (!io) {
+      throw new Error('Socket.IO client not found');
+    }
   };
 
   /**

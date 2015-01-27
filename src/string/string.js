a4ea5afd1358936e9cc951bcbf3759f92e2c305b
Eduardo Lundgren <eduardolundgren@gmail.com>
2015-01-26 21:00:35 -0300

    Adds lfr.string.collapseBreakingSpaces and lfr.string.replaceInterval utilities

--- src/string/string.js
+++ src/string/string.js
@@ -4,6 +4,17 @@
   lfr.string = lfr.string || {};
 
   /**
+   * Removes the breaking spaces from the left and right of the string and
+   * collapses the sequences of breaking spaces in the middle into single spaces.
+   * The original and the result strings render the same way in HTML.
+   * @param {string} str A string in which to collapse spaces.
+   * @return {string} Copy of the string with normalized breaking spaces.
+   */
+  lfr.string.collapseBreakingSpaces = function(str) {
+    return str.replace(/[\t\r\n ]+/g, ' ').replace(/^[\t\r\n ]+|[\t\r\n ]+$/g, '');
+  };
+
+  /**
    * Calculates the hashcode for a string. The hashcode value is computed by
    * the sum algorithm: s[0]*31^(n-1) + s[1]*31^(n-2) + ... + s[n-1]. A nice
    * property of using 31 prime is that the multiplication can be replaced by
@@ -19,6 +30,19 @@
       hash %= 0x100000000;
     }
     return hash;
+  };
+
+  /**
+   * Replaces interval into the string with specified value, e.g.
+   * `replaceInterval("abcde", 1, 4, "")` returns "ae".
+   * @param {string} str The input string.
+   * @param {Number} start Start interval position to be replaced.
+   * @param {Number} end End interval position to be replaced.
+   * @param {string} value The value that replaces the specified interval.
+   * @return {string}
+   */
+  lfr.string.replaceInterval = function(str, start, end, value) {
+    return str.substring(0, start) + value + str.substring(end);
   };
 
 }());
--- test/string/string.js
+++ test/string/string.js
@@ -7,4 +7,12 @@ describe('string', function() {
   it('should compute string hashcode', function() {
     assert.strictEqual(101574, lfr.string.hashCode('foo'));
   });
+
+  it('should collapse breaking spaces', function() {
+    assert.strictEqual('foo bar', lfr.string.collapseBreakingSpaces('   foo   bar   '));
+  });
+
+  it('should replace interval', function() {
+    assert.strictEqual('ae', lfr.string.replaceInterval('abcde', 1, 4, ''));
+  });
 });

77d025397c6bc1949987c2333690633192a8cc34
Maira Bello <maira.araujo@liferay.com>
2015-03-17 10:54:39 -0300

    Adds missing gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,56 @@
 'use strict';
 
+var del = require('del');
+var gulp = require('gulp');
 var pkg = require('./package.json');
+var plugins = require('gulp-load-plugins')();
 var registerTasks = require('alloyui-tasks');
+var runSequence = require('run-sequence');
 
 registerTasks({
   bundleFileName: 'aui.js',
   corePathFromSoy: '../',
-  pkg: pkg,
   shouldSkipSoyTemplatesGeneration: true
 });
+
+gulp.task('build', function(done) {
+  runSequence('clean', ['build:globals', 'build:min'], done);
+});
+
+gulp.task('build:min', ['build:globals'], function() {
+  return gulp.src('build/aui.js')
+    .pipe(plugins.rename('aui-min.js'))
+    .pipe(plugins.uglify({
+      compress: {drop_console: true},
+      preserveComments: 'some'
+    }))
+    .pipe(banner())
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('clean', function(done) {
+  del(['build'], done);
+});
+
+gulp.task('lint', function() {
+  return gulp.src(['src/**/*.js', 'test/**/*.js'])
+    .pipe(plugins.jshint())
+    .pipe(plugins.jshint.reporter(require('jshint-stylish')));
+});
+
+function banner() {
+  var stamp = [
+    '/**',
+    ' * <%= pkg.name %> - <%= pkg.description %>',
+    ' * @version v<%= pkg.version %>',
+    ' * @author <%= pkg.author.name %> <<%= pkg.author.email %>>',
+    ' * @link http://liferay.com',
+    ' * @license BSD',
+    ' */',
+    ''
+  ].join('\n');
+
+  return plugins.header(stamp, {
+    pkg: pkg
+  });
+}

5133fa4a8491e2d85f7fb8e0ed4c55798432dee0
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2015-03-20 12:12:43 -0300

    Adds dom position helper to gets the size of an element and its position relative to the viewport

--- src/dom/position.js
+++ src/dom/position.js
@@ -5,6 +5,17 @@
  */
 class position {
   /**
+   * Gets the size of an element and its position relative to the viewport.
+   * @param {!Element} node
+   * @return {!DOMRect} The returned value is a DOMRect object which is the
+   *     union of the rectangles returned by getClientRects() for the element,
+   *     i.e., the CSS border-boxes associated with the element.
+   */
+  static getRegion(node) {
+    return node.getBoundingClientRect();
+  }
+
+  /**
    * Gets the dimensions of the viewport excluding scrollbar.
    * @param {Window=} opt_window Optional window element to test.
    * @return {!Object} Object with values 'width' and 'height'.
@@ -16,7 +27,7 @@ class position {
       height: el.clientHeight,
       width: el.clientWidth
     };
-  };
+  }
 }
 
 export default position;
--- test/dom/position.js
+++ test/dom/position.js
@@ -4,20 +4,40 @@ import dom from '../../src/dom/dom';
 import position from '../../src/dom/position';
 
 describe('position', function() {
+  var iframeAbsolute = dom.buildFragment('<iframe id="iframeAbsolute" style="height:10000px;width:10000px;padding:0;margin:0;border-width:0;position:absolute;top:0;left:0;"></iframe>').firstChild;
+  var iframeStatic = dom.buildFragment('<iframe id="iframeStatic" style="height:10000px;width:10000px;padding:0;margin:0;border-width:0;position:relative;"></iframe>').firstChild;
+
   before(function() {
-    dom.enterDocument('<iframe id="iframe1" style="height:10000px;width:10000px;"></iframe>');
+    dom.enterDocument(iframeAbsolute);
+    dom.enterDocument(iframeStatic);
+  });
+
+  after(function() {
+    dom.exitDocument(iframeAbsolute);
+    dom.exitDocument(iframeStatic);
   });
 
   describe('viewport', function() {
     it('should check iframe viewport size', function() {
-      var iframe = document.getElementById('iframe1');
-      assert.strictEqual(10000, position.getViewportSize(iframe.contentWindow).height);
-      assert.strictEqual(10000, position.getViewportSize(iframe.contentWindow).width);
+      assert.strictEqual(10000, position.getViewportSize(iframeStatic.contentWindow).height);
+      assert.strictEqual(10000, position.getViewportSize(iframeStatic.contentWindow).width);
     });
 
     it('should check window viewport size', function() {
       assert.ok(window.document.documentElement.scrollHeight > position.getViewportSize().height);
       assert.ok(window.document.documentElement.scrollWidth > position.getViewportSize().width);
+    });
+  });
+
+  describe('region', function() {
+    it('should get node region', function() {
+      var region = position.getRegion(iframeAbsolute);
+      assert.strictEqual(10000, region.height);
+      assert.strictEqual(10000, region.width);
+      assert.strictEqual(10000, region.right);
+      assert.strictEqual(10000, region.bottom);
+      assert.strictEqual(0, region.left);
+      assert.strictEqual(0, region.top);
     });
   });
 });

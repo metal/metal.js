afb17884a85ce2fbd0366bbb0a452f3c0cdd7fbb
Maira Bello <maira.araujo@liferay.com>
2015-04-02 17:35:53 -0300

    Adds watch:globals gulp task

--- tasks/lib/build.js
+++ tasks/lib/build.js
@@ -5,6 +5,7 @@ var gulp = require('gulp');
 var normalizeOptions = require('./options');
 var sourcemaps = require('gulp-sourcemaps');
 var babel = require('gulp-babel');
+var runSequence = require('run-sequence');
 var transpile = require('gulp-es6-module-transpiler');
 var plugins = require('gulp-load-plugins')();
 
@@ -12,7 +13,11 @@ module.exports = function(options) {
   options = normalizeOptions(options);
   var taskPrefix = options.taskPrefix;
 
-  gulp.task(taskPrefix + 'build:globals', [taskPrefix + 'soy'], function() {
+  gulp.task(taskPrefix + 'build:globals', [taskPrefix + 'soy'], function(done) {
+    runSequence(taskPrefix + 'soy', taskPrefix + 'globals', done);
+  });
+
+  gulp.task(taskPrefix + 'globals', function() {
     return gulp.src(options.buildSrc)
       .pipe(sourcemaps.init())
       .pipe(transpile({
@@ -32,6 +37,11 @@ module.exports = function(options) {
       }))
       .pipe(sourcemaps.write('./'))
       .pipe(gulp.dest(options.buildDest));
+  });
+
+  gulp.task(taskPrefix + 'watch:globals', function(done) { // jshint ignore:line
+    gulp.watch(options.buildSrc, [taskPrefix + 'globals']);
+    gulp.watch(options.soySrc, [taskPrefix + 'soy']);
   });
 };
 

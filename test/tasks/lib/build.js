3f5559f653c2ae3418fd06e64ac1ea1cd85e6950
Maira Bello <maira.araujo@liferay.com>
2015-04-06 12:12:43 -0300

    Adds test for watch:globals task

--- test/tasks/lib/build.js
+++ test/tasks/lib/build.js
@@ -12,7 +12,9 @@ describe('Build Tasks', function() {
     this.initialCwd_ = process.cwd();
     process.chdir(path.join(__dirname, 'assets'));
 
-    gulp.task('soy', sinon.stub().yields());
+    gulp.task('soy', function(done) {
+      done();
+    });
   });
 
   after(function() {
@@ -52,5 +54,22 @@ describe('Build Tasks', function() {
       assert.strictEqual(1, console.error.callCount);
       done();
     });
+  });
+
+  it('should watch for file changes on watch:globals', function(done) {
+    registerTasks();
+    sinon.stub(gulp, 'watch');
+
+    gulp.start('watch:globals', function() {
+      assert.strictEqual(2, gulp.watch.callCount);
+      assert.strictEqual('src/**/*.js', gulp.watch.args[0][0]);
+      assert.deepEqual(['globals'], gulp.watch.args[0][1]);
+      assert.strictEqual('src/**/*.soy', gulp.watch.args[1][0]);
+      assert.deepEqual(['soy'], gulp.watch.args[1][1]);
+
+      gulp.watch.restore();
+      done();
+    });
+    gulp.stop();
   });
 });

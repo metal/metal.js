b65da27defa3a28e341e46043a71ba4693f149be
Zeno Rocha <hi@zenorocha.com>
2015-06-05 17:00:21 -0300

    Adds compass build

--- packages/metaljs.com/gulpfile.js
+++ packages/metaljs.com/gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var compass = require('gulp-compass');
 var soynode = require('gulp-soynode');
 
 gulp.task('soy', function() {
@@ -7,4 +8,14 @@ gulp.task('soy', function() {
 			renderSoyWeb: true
 		}))
 		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('styles', function() {
+	return gulp.src('src/public/styles/*.scss')
+		.pipe(compass({
+			css: 'dist/public/styles',
+			sass: 'src/public/styles',
+			image: 'dist/public/images'
+		}))
+		.pipe(gulp.dest('dist/public/styles'));
 });

2c007e328eb652e22b8daf85636b3846d0438cf9
Maira Bello <maira.araujo@liferay.com>
2016-01-29 11:45:20 -0300

    Adds index.js file for easy usage through node

---
+++ src/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import core from './core';
+import array from './array/array';
+import async from './async/async';
+import Disposable from './disposable/Disposable';
+import EventEmitter from './events/EventEmitter';
+import EventEmitterProxy from './events/EventEmitterProxy';
+import EventHandle from './events/EventHandle';
+import EventHandler from './events/EventHandler';
+import object from './object/object';
+import string from './string/string';
+
+export { core };
+export { array };
+export { async };
+export { Disposable };
+export { EventEmitter };
+export { EventEmitterProxy };
+export { EventHandle };
+export { EventHandler };
+export { object };
+export { string };

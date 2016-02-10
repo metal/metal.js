342e0b419cf482f6ec9440556af8c59dfd22aa74
Maira Bello <maira.araujo@liferay.com>
2016-02-10 13:40:35 -0300

    Moves and renames main file
    We want the globals build to use the name "dom" for the named exports global variable, so the main file has to have that name too.oves main file to different directory.

---
+++ src/all/dom.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import dom from '../dom';
+import DomEventEmitterProxy from '../DomEventEmitterProxy';
+import DomEventHandle from '../DomEventHandle';
+import features from '../features';
+import globalEval from '../globalEval';
+import globalEvalStyles from '../globalEvalStyles';
+import '../events';
+
+export default dom;
+export { dom, DomEventEmitterProxy, DomEventHandle, features, globalEval, globalEvalStyles };

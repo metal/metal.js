441109c7854babbcdb8195778578e6d99ff78a90
Maira Bello <maira.araujo@liferay.com>
2016-03-28 15:05:22 -0300

    Removes SurfaceRenderer and SurfaceCollector

--- src/all/component.js
+++ src/all/component.js
@@ -5,8 +5,6 @@ import ComponentCollector from '../ComponentCollector';
 import ComponentRegistry from '../ComponentRegistry';
 import ComponentRenderer from '../ComponentRenderer';
 import EventsCollector from '../EventsCollector';
-import SurfaceCollector from '../surfaces/SurfaceCollector';
-import SurfaceRenderer from '../surfaces/SurfaceRenderer';
 
 export default Component;
 export {
@@ -14,7 +12,5 @@ export {
 	ComponentCollector,
 	ComponentRegistry,
 	ComponentRenderer,
-	EventsCollector,
-	SurfaceCollector,
-	SurfaceRenderer
+	EventsCollector
 };

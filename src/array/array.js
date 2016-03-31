f4dbf92e71a2ac4f3fcfc6a81c0639abddc1e390
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2016-03-31 16:52:01 -0300

    Minor performance improvement for array-equality check

--- src/array/array.js
+++ src/array/array.js
@@ -10,12 +10,15 @@ class array {
 	 * @return {boolean}
 	 */
 	static equal(arr1, arr2) {
+		if (arr1.length !== arr2.length) {
+			return false;
+		}
 		for (var i = 0; i < arr1.length; i++) {
 			if (arr1[i] !== arr2[i]) {
 				return false;
 			}
 		}
-		return arr1.length === arr2.length;
+		return true;
 	}
 
 	/**

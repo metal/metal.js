19590aa1a9e78a60a0cf20298d76d7e50ab5a60f
Maira Bello <maira.araujo@liferay.com>
2016-04-05 11:26:28 -0300

    Adds simple implementation

--- src/JSX.js
+++ src/JSX.js
@@ -1,6 +1,47 @@
 'use strict';
 
-class JSX {
+import IncrementalDomRenderer from 'metal-incremental-dom';
+
+/**
+ * Allows components to use JSX templates to render their contents. Usage
+ * example:
+ *
+ * class MyComp extends Component {
+ *   jsx() {
+ *     <div class="my-comp">Hello World</div>;
+ *   }
+ * }
+ * JSX.register(MyComp);
+ */
+class JSX extends IncrementalDomRenderer {
+	/**
+	 * @inheritDoc
+	 */
+	constructor(comp) {
+		super(comp);
+		this.fn_ = comp.jsx && comp.jsx.bind(comp);
+	}
+
+	/**
+	 * Overrides the default method from `IncrementalDomRenderer` so the
+	 * component's JSX template can be used for rendering.
+	 * @override
+	 */
+	renderIncDom() {
+		if (this.fn_) {
+			this.fn_();
+		} else {
+			super.renderIncDom();
+		}
+	}
+
+	/**
+	 * Registers the given component constructor to use JSX templates.
+	 * @param {!Function} componentCtor
+	 */
+	static register(componentCtor) {
+		componentCtor.RENDERER = JSX;
+	}
 }
 
 export default JSX;

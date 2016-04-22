a40e14f31459814077a543efb121b99b0efdd2c1
Maira Bello <maira.araujo@liferay.com>
2016-04-22 15:23:28 -0300

    Uses local karma to run tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 'use strict';
 
 var gulp = require('gulp');
+var karma = require('karma');
 var metal = require('gulp-metal');
 var runSequence = require('run-sequence');
 
 metal.registerTasks({
+	karma: karma,
 	testNodeSrc: [
 		'env/test/node.js',
 		'test/**/*.js'

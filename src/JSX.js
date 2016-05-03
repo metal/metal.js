a51b0c77709dfa387cb9f64e55415f79f0590c3b
Maira Bello <maira.araujo@liferay.com>
2016-05-03 12:14:22 -0300

    Removes code that is already inside IncrementalDomRenderer - Fixes #5

--- src/JSX.js
+++ src/JSX.js
@@ -16,14 +16,6 @@ import IncrementalDomRenderer from 'metal-incremental-dom';
  */
 class JSX extends IncrementalDomRenderer {
 	/**
-	 * @inheritDoc
-	 */
-	constructor(comp) {
-		super(comp);
-		this.fn_ = comp.render && comp.render.bind(comp);
-	}
-
-	/**
 	 * Overrides the original method so the "children" function can be
 	 * automatically rendered through jsx.
 	 * @param {!Array<!{name: string, args: !Array}>} calls
@@ -34,19 +26,6 @@ class JSX extends IncrementalDomRenderer {
 	buildChildrenFn_(calls) {
 		var fn = super.buildChildrenFn_(calls);
 		return iDOMHelpers.jsxWrapper(fn);
-	}
-
-	/**
-	 * Overrides the default method from `IncrementalDomRenderer` so the
-	 * component's JSX template can be used for rendering.
-	 * @override
-	 */
-	renderIncDom() {
-		if (this.fn_) {
-			this.fn_();
-		} else {
-			super.renderIncDom();
-		}
 	}
 }
 

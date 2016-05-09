6f68f6a3b693dcb2c393cb3e7ec86ffb963e403c
Maira Bello <maira.araujo@liferay.com>
2016-05-09 15:11:08 -0300

    Uses CodeMirror.defaults

--- packages/metaljs.com/src/public/scripts/highlight.js
+++ packages/metaljs.com/src/public/scripts/highlight.js
@@ -1,14 +1,13 @@
+CodeMirror.defaults.lineNumbers = true;
+CodeMirror.defaults.matchBrackets = true;
+CodeMirror.defaults.theme = 'material';
+CodeMirror.defaults.viewportMargin = Infinity;
+CodeMirror.defaults.readOnly = true;
+
 var code = document.querySelectorAll('.code');
 
 for (var i = 0; i < code.length; i++) {
 	CodeMirror.fromTextArea(code[i], {
-		mode: code[i].dataset.mode ? 'text/' + code[i].dataset.mode : '',
-		lineNumbers: true,
-		matchBrackets: true,
-		indentUnit: 2,
-		indentWithTabs: false,
-		theme: 'material',
-		viewportMargin: Infinity,
-		readOnly: true
+		mode: code[i].dataset.mode ? 'text/' + code[i].dataset.mode : ''
 	});
 }

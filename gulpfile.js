d97afadfc53ef945888ee9d217f88c1ba7e1c482
Maira Bello <maira.araujo@liferay.com>
2016-05-23 10:15:54 -0300

    Adds gulp task to create incremental dom file to be used - Fixes #6
    The task removes commonjs and requirejs handling, as well as the source map url.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,26 @@
 'use strict';
 
+var gulp = require('gulp');
 var metal = require('gulp-metal');
+var replace = require('gulp-replace');
+var wrapper = require('gulp-wrapper');
 
 metal.registerTasks({
 	bundleCssFileName: 'incrementalDom.css',
 	bundleFileName: 'incrementalDom.js',
 	moduleName: 'metal-incrementalDom',
 	noSoy: true
+});
+
+gulp.task('build:incdom', function() {
+	return gulp.src('node_modules/incremental-dom/dist/incremental-dom.js')
+    .pipe(replace('\n  typeof exports === \'object\' && typeof module !== \'undefined\' ? factory(exports) :', ''))
+		.pipe(replace('\n  typeof define === \'function\' && define.amd ? define([\'exports\'], factory) :', ''))
+		.pipe(replace('}(this, function (exports) {', '}(window, function (exports) {'))
+		.pipe(replace('\n//# sourceMappingURL=incremental-dom.js.map', ''))
+		.pipe(wrapper({
+			header: '/* jshint ignore:start */\n',
+			footer: '/* jshint ignore:end */\n'
+		}))
+    .pipe(gulp.dest('src'));
 });

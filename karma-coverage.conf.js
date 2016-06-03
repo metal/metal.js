c0158a94d2da05a61d1138969b5e050298346c82
Maira Bello <maira.araujo@liferay.com>
2016-06-03 15:38:31 -0300

    Adds test coverage karma config

---
+++ karma-coverage.conf.js
@@ -0,0 +1,55 @@
+'use strict';
+
+module.exports = function (config) {
+  config.set({
+    frameworks: ['browserify', 'mocha', 'chai', 'sinon', 'source-map-support'],
+
+    files: [
+      // Since all files will be added, we need to ensure manually that these
+      // will be added first.
+      {pattern: 'packages/metal-incremental-dom/src/incremental-dom.js', watched: false, included: true, served: true},
+      {pattern: 'packages/metal-incremental-dom/lib/incremental-dom.js', watched: false, included: true, served: true},
+      {pattern: 'packages/metal-soy/node_modules/metal-soy-bundle/lib/bundle.js', watched: false, included: true, served: true},
+      {pattern: 'packages/metal-soy/node_modules/html2incdom/lib/*.js', watched: false, included: true, served: true},
+
+      {pattern: 'packages/metal*/test/**/*.js', watched: false, included: true, served: true},
+      {pattern: 'packages/metal-dom/fixtures/*', watched: true, included: false, served: true}
+    ],
+
+    preprocessors: {
+      'packages/metal-incremental-dom/src/incremental-dom.js': ['browserify'],
+      'packages/metal-incremental-dom/lib/incremental-dom.js': ['browserify'],
+      'packages/metal-soy/node_modules/metal-soy-bundle/lib/bundle.js': ['browserify'],
+      'packages/metal-soy/node_modules/html2incdom/lib/*.js': ['browserify'],
+      'packages/metal*/test/**/*.js': ['browserify']
+    },
+
+    browsers: ['Chrome'],
+
+		browserify: {
+        debug: true,
+        transform: [ require('browserify-istanbul')({
+	        instrumenter: require('isparta'),
+	        ignore: [
+						'**/packages/metal-incremental-dom/**/incremental-dom.js',
+						'**/packages/metal-jsx/**/iDOMHelpers.js'
+					]
+	      }), 'babelify' ]
+    },
+
+    reporters: ['coverage', 'progress'],
+
+    coverageReporter: {
+			reporters: [
+				{type: 'lcov', subdir: 'lcov'},
+				{type: 'text-summary'}
+			]
+    },
+
+    autoWatch: true,
+
+    proxies: {
+      '/fixtures/': '/base/packages/metal-dom/fixtures/'
+    }
+  });
+};

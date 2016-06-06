badd4b0d1f0454935cd911331f00e1e0ce055b11
Maira Bello <maira.araujo@liferay.com>
2016-06-06 11:56:52 -0300

    Makes sure that changes to src file reflect on watched tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 'use strict';
 
+var babel = require('gulp-babel');
 var compileSoy = require('metal-tools-soy/lib/pipelines/compileSoy');
 var gulp = require('gulp');
+var karma = require('karma');
 var metal = require('gulp-metal');
+var path = require('path');
 var replace = require('gulp-replace');
 
 var codeGlobs = [
@@ -35,3 +38,39 @@ gulp.task('soy', function() {
 		.pipe(replace('metal-component/src/Component', 'metal-component'))
 		.pipe(gulp.dest('packages/metal-soy/test'));
 });
+
+var changedJsSrc;
+gulp.task('compile', function() {
+	return gulp.src(changedJsSrc)
+		.pipe(babel({
+			presets: ['es2015']
+		}))
+		.pipe(gulp.dest(calcDestDir(changedJsSrc)));
+});
+
+// We need to override gulp-metal's default test:watch task so that it will
+// update lib files when the related src files change.
+gulp.task('test:watch', ['soy'], function(done) { // jshint ignore:line
+	gulp.watch('packages/metal-soy/test/**/*.soy', ['soy']);
+	var jsWatcher = gulp.watch('packages/metal*/src/**/*.js', ['compile']);
+	jsWatcher.on('change', function(event) {
+		changedJsSrc = event.path;
+	});
+
+	new karma.Server(
+		{
+			configFile: path.resolve('karma.conf.js')
+		}
+	).start();
+});
+
+function calcDestDir(src) {
+	var relative = path.relative(path.resolve('packages'), src);
+	var index = relative.indexOf(path.sep);
+	return path.dirname(path.join(
+		path.resolve('packages'),
+		relative.substr(0, index),
+		'lib',
+		relative.substr(index + 5)
+	));
+}

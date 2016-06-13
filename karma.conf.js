7d8871878dab4772f15782bbeefb302dfea566ee
Maira Bello <maira.araujo@liferay.com>
2016-06-13 15:42:11 -0300

    Makes karma.conf.js reuse karma-sauce.conf.js
    It only adds source-map-support to it.

--- karma.conf.js
+++ karma.conf.js
@@ -1,70 +1,8 @@
 'use strict';
 
+var karmaConf = require('./karma-sauce.conf');
+
 module.exports = function(config) {
-	config.set({
-		frameworks: ['browserify', 'mocha', 'chai', 'sinon', 'source-map-support'],
-
-		files: [
-			// Since all files will be added, we need to ensure manually that these
-			// will be added first.
-			{
-				pattern: 'packages/metal-incremental-dom/src/incremental-dom.js',
-				watched: false,
-				included: true,
-				served: true
-			},
-			{
-				pattern: 'packages/metal-incremental-dom/lib/incremental-dom.js',
-				watched: false,
-				included: true,
-				served: true
-			},
-			{
-				pattern: 'packages/metal-soy/node_modules/metal-soy-bundle/lib/bundle.js',
-				watched: false,
-				included: true,
-				served: true
-			},
-			{
-				pattern: 'packages/metal-soy/node_modules/html2incdom/lib/*.js',
-				watched: false,
-				included: true,
-				served: true
-			},
-
-			{
-				pattern: 'packages/metal*/test/**/*.js',
-				watched: false,
-				included: true,
-				served: true
-			},
-			{
-				pattern: 'packages/metal-dom/fixtures/*',
-				watched: true,
-				included: false,
-				served: true
-			}
-		],
-
-		preprocessors: {
-			'packages/metal-incremental-dom/src/incremental-dom.js': ['browserify'],
-			'packages/metal-incremental-dom/lib/incremental-dom.js': ['browserify'],
-			'packages/metal-soy/node_modules/metal-soy-bundle/lib/bundle.js': ['browserify'],
-			'packages/metal-soy/node_modules/html2incdom/lib/*.js': ['browserify'],
-			'packages/metal*/test/**/*.js': ['browserify']
-		},
-
-		browsers: ['Chrome'],
-
-		browserify: {
-			debug: true,
-			transform: ['babelify']
-		},
-
-		autoWatch: true,
-
-		proxies: {
-			'/fixtures/': '/base/packages/metal-dom/fixtures/'
-		}
-	});
+	karmaConf(config);
+	config.frameworks.push('source-map-support');
 };

8d4137c89230edf51b89c267472b87d789563b41
Maira Bello <maira.araujo@liferay.com>
2016-06-21 16:07:06 -0300

    Removes EventsCollector
    This is not being used anywhere anymore.

--- packages/metal-component/src/all/component.js
+++ packages/metal-component/src/all/component.js
@@ -3,12 +3,10 @@
 import Component from '../Component';
 import ComponentRegistry from '../ComponentRegistry';
 import ComponentRenderer from '../ComponentRenderer';
-import EventsCollector from '../EventsCollector';
 
 export default Component;
 export {
 	Component,
 	ComponentRegistry,
-	ComponentRenderer,
-	EventsCollector
+	ComponentRenderer
 };

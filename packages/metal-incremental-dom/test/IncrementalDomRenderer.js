6f62854b652c4a86e2cd604443aa164101fe53c7
Maira Bello <maira.araujo@liferay.com>
2016-06-22 11:31:47 -0300

    Adds test case to guarantee that components with same ref as unused components won't be disposed

--- packages/metal-incremental-dom/test/IncrementalDomRenderer.js
+++ packages/metal-incremental-dom/test/IncrementalDomRenderer.js
@@ -2121,6 +2121,46 @@ describe('IncrementalDomRenderer', function() {
 				done();
 			});
 		});
+
+		it('should not dispose new sub component with same ref as unused sub component', function(done) {
+			class TestChildComponent extends Component {
+				render() {
+					IncrementalDOM.elementVoid('child');
+				}
+			}
+			TestChildComponent.RENDERER = IncrementalDomRenderer;
+
+			class TestComponent extends Component {
+				render() {
+					IncDom.elementOpen('div');
+					if (this.switch) {
+						IncDom.elementVoid(TestChildComponent, null, null, 'ref', 'child');
+					} else {
+						IncDom.elementVoid(ChildComponent, null, null, 'ref', 'child');
+					}
+					IncDom.elementClose('div');
+				}
+			}
+			TestComponent.RENDERER = IncrementalDomRenderer;
+			TestComponent.STATE = {
+				switch: {
+				}
+			};
+			component = new TestComponent();
+			var child = component.components.child;
+			assert.ok(child instanceof ChildComponent);
+
+			component.switch = true;
+			component.once('stateSynced', function() {
+				var newChild = component.components.child;
+				assert.ok(newChild instanceof TestChildComponent);
+				assert.notStrictEqual(child, newChild);
+				assert.ok(child.isDisposed());
+				assert.ok(!newChild.isDisposed());
+				assert.strictEqual(newChild.element, component.element.childNodes[0]);
+				done();
+			});
+		});
 	});
 
 	describe('Function - shouldUpdate', function() {

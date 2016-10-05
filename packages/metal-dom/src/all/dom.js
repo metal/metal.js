0b67b279c7f401419973b1a8d2c52b39292e0de3
Maira Bello <maira.araujo@liferay.com>
2016-10-05 18:11:19 -0300

    Adds back default export for dom.js file
    That's important for backwards compatibility for projects that were using the full path instead of an alias.

--- packages/metal-dom/src/all/dom.js
+++ packages/metal-dom/src/all/dom.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import * as dom from '../dom';
+import dom from '../dom';
 import domData from '../domData';
 import DomEventEmitterProxy from '../DomEventEmitterProxy';
 import DomEventHandle from '../DomEventHandle';
@@ -11,10 +11,4 @@ import '../events';
 
 export * from '../dom';
 export { domData, DomEventEmitterProxy, DomEventHandle, features, globalEval, globalEvalStyles };
-
-// This is for backwards compatibility, making sure that old imports for the
-// "dom" object still work. It's best to use the named exports for each function
-// instead though, since that allows bundlers like Rollup to reduce the bundle
-// size by removing unused code.
 export default dom;
-export { dom };

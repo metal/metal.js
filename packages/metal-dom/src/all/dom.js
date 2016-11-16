6bf56ef43654ce1cd43bfb80a795517872ac4f94
Fernando Souza <fernando.souza@liferay.com>
2016-11-16 18:59:38 -0300

    Adds keyConstants file

--- packages/metal-dom/src/all/dom.js
+++ packages/metal-dom/src/all/dom.js
@@ -4,11 +4,12 @@ import dom from '../dom';
 import domData from '../domData';
 import DomEventEmitterProxy from '../DomEventEmitterProxy';
 import DomEventHandle from '../DomEventHandle';
+import * as KEYMAP from '../keyConstants';
 import features from '../features';
 import globalEval from '../globalEval';
 import globalEvalStyles from '../globalEvalStyles';
 import '../events';
 
 export * from '../dom';
-export { domData, DomEventEmitterProxy, DomEventHandle, features, globalEval, globalEvalStyles };
+export { domData, DomEventEmitterProxy, DomEventHandle, features, globalEval, globalEvalStyles, KEYMAP };
 export default dom;

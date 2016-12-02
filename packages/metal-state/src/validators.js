b53779c4d00538ce29126f8b77dc0748064c2e64
Maira Bello <maira.araujo@liferay.com>
2016-12-02 13:02:21 -0300

    Cleans up "validators" code

--- packages/metal-state/src/validators.js
+++ packages/metal-state/src/validators.js
@@ -1,6 +1,12 @@
 'use strict';
 
-import { getFunctionName, isDef, isDefAndNotNull, isNull } from 'metal';
+import { getFunctionName, isDefAndNotNull } from 'metal';
+
+const ERROR_ARRAY_OF_TYPE = 'Expected an array of single type.';
+const ERROR_OBJECT_OF_TYPE = 'Expected object of one type.';
+const ERROR_ONE_OF = 'Expected one of given values.';
+const ERROR_ONE_OF_TYPE = 'Expected one of given types.';
+const ERROR_SHAPE_OF = 'Expected object with a specific shape.';
 
 /**
  * Provides access to various type validators that will return an
@@ -10,61 +16,55 @@ import { getFunctionName, isDef, isDefAndNotNull, isNull } from 'metal';
  */
 const validators = {
 	any: () => () => true,
-	array: validateType('array'),
-	bool: validateType('boolean'),
-	func: validateType('function'),
-	number: validateType('number'),
-	object: validateType('object'),
-	string: validateType('string'),
+	array: buildTypeValidator('array'),
+	bool: buildTypeValidator('boolean'),
+	func: buildTypeValidator('function'),
+	number: buildTypeValidator('number'),
+	object: buildTypeValidator('object'),
+	string: buildTypeValidator('string'),
 
 	/**
-	 * Creates a validator that checks the values of an array against a type.
-	 * @param {function()} validator Type validator to check each index against.
-	 * @return {function()} Validator.
+	 * Creates a validator that checks that the value it receives is an array
+	 * of items, and that all of the items pass the given validator.
+	 * @param {!function()} validator Validator to check each item against.
+	 * @return {!function()}
 	 */
 	arrayOf: function(validator) {
 		return maybe((value, name, context) => {
-			var result = validators.array(value, name, context);
+			const result = validators.array(value, name, context);
 			if (isInvalid(result)) {
 				return result;
 			}
-			for (var i = 0; i < value.length; i++) {
-				if (isInvalid(validator(value[i], name, context))) {
-					return composeError('Expected an array of single type', name, context);
-				}
-			}
-			return true;
+			return validateArrayItems(validator, value, name, context);
 		});
 	},
 
 	/**
-	 * Creates a validator that compares a value to a specific class.
-	 * @param {function()} expectedClass Class to check value against.
-	 * @return {function()} Validator.
+	 * Creates a validator that checks if a value is an instance of a given class.
+	 * @param {!function()} expectedClass Class to check value against.
+	 * @return {!function()}
 	 */
 	instanceOf: function(expectedClass) {
 		return maybe((value, name, context) => {
-			if (!(value instanceof expectedClass)) {
-				return composeError(
-					`Expected instance of ${expectedClass}`,
-					name,
-					context
-				);
+			if (value instanceof expectedClass) {
+				return true;
 			}
-			return true;
+			const msg = `Expected instance of ${expectedClass}`;
+			return composeError(msg, name, context);
 		});
 	},
 
 	/**
-	 * Creates a validator that checks the values of an object against a type.
-	 * @param {function()} typeValidator Validator to check value against.
-	 * @return {function()} Validator.
+	 * Creates a validator that checks that the value it receives is an object,
+	 * and that all values within that object pass the given validator.
+	 * @param {!function()} validator Validator to check each object value against.
+	 * @return {!function()}
 	 */
-	objectOf: function(typeValidator) {
+	objectOf: function(validator) {
 		return maybe((value, name, context) => {
 			for (let key in value) {
-				if (isInvalid(typeValidator(value[key]))) {
-					return composeError('Expected object of one type', name, context);
+				if (isInvalid(validator(value[key]))) {
+					return composeError(ERROR_OBJECT_OF_TYPE, name, context);
 				}
 			}
 			return true;
@@ -72,9 +72,10 @@ const validators = {
 	},
 
 	/**
-	 * Creates a validator that checks equality against specific values.
+	 * Creates a validator that checks if the received value matches one of the
+	 * given values.
 	 * @param {!Array} arrayOfValues Array of values to check equality against.
-	 * @return {function()} Validator.
+	 * @return {!function()}
 	 */
 	oneOf: function(arrayOfValues) {
 		return maybe((value, name, context) => {
@@ -82,24 +83,18 @@ const validators = {
 			if (isInvalid(result)) {
 				return result;
 			}
-
-			for (let i = 0; i < arrayOfValues.length; i++) {
-				const oneOfValue = arrayOfValues[i];
-				if (value === oneOfValue) {
-					return true;
-				}
-			}
-
-			return composeError('Expected one of given values.', name, context);
+			return arrayOfValues.indexOf(value) === -1 ?
+				composeError(ERROR_ONE_OF, name, context) :
+				true;
 		});
 	},
 
 	/**
-	 * Creates a validator that checks a value against multiple types and only has
-	 * to pass one.
+	 * Creates a validator that checks if the received value matches one of the
+	 * given types.
 	 * @param {!Array} arrayOfTypeValidators Array of validators to check value
 	 *     against.
-	 * @return {function()} Validator.
+	 * @return {!function()}
 	 */
 	oneOfType: function(arrayOfTypeValidators) {
 		return maybe((value, name, context) => {
@@ -113,15 +108,15 @@ const validators = {
 					return true;
 				}
 			}
-
-			return composeError('Expected one of given types.', name, context);
+			return composeError(ERROR_ONE_OF_TYPE, name, context);
 		});
 	},
 
 	/**
-	 * Creates a validator that checks the shape of an object.
-	 * @param {!Object} shape An object containing type validators for each key.
-	 * @return {function()} Validator.
+	 * Creates a validator that checks if the received value is an object, and
+	 * that its contents match the given shape.
+	 * @param {!Object} shape An object containing validators for each key.
+	 * @return {!function()}
 	 */
 	shapeOf: function(shape) {
 		return maybe((value, name, context) => {
@@ -131,40 +126,56 @@ const validators = {
 			}
 
 			for (let key in shape) {
-				let required = false;
 				let validator = shape[key];
+				let required = false;
 				if (validator.config) {
 					required = validator.config.required;
 					validator = validator.config.validator;
 				}
 				if ((required && !isDefAndNotNull(value[key])) ||
 					isInvalid(validator(value[key]))) {
-					return composeError(
-						'Expected object with a specific shape',
-						name,
-						context
-					);
+					return composeError(ERROR_SHAPE_OF, name, context);
 				}
 			}
-
 			return true;
 		});
 	}
 };
 
 /**
+ * Creates a validator that checks against a specific primitive type.
+ * @param {string} expectedType Type to check against.
+ * @return {!function()} Function that runs the validator if called with
+ *     arguments, or just returns it otherwise. This means that when using a
+ *     type validator in `State` it may be just passed directly (like
+ *     `validators.bool`), or called with no args (like `validators.bool()`).
+ *     That's done to allow all validators to be used consistently, since some
+ *     (like `arrayOf`) always require that you call the function before
+ *     receiving the actual validator. Type validators don't need the call, but
+ *     work if it's made anyway.
+ */
+function buildTypeValidator(expectedType) {
+	const validatorFn = maybe(validateType.bind(null, expectedType));
+	return (...args) => {
+		if (args.length === 0) {
+			return validatorFn;
+		} else {
+			return validatorFn(...args);
+		}
+	};
+}
+
+/**
  * Composes a warning a warning message.
  * @param {string} error Error message to display to console.
  * @param {?string} name Name of state property that is giving the error.
- * @param {Object} context
- * @return {!Error} Instance of Error class.
+ * @param {Object} context The property's owner.
+ * @return {!Error}
  */
 function composeError(error, name, context) {
 	const compName = context ? getFunctionName(context.constructor) : null;
-	var renderer = context && context.getRenderer && context.getRenderer();
-	const parent = renderer && renderer.getParent ?
-		context.getRenderer().getParent() :
-		null;
+	const renderer = context && context.getRenderer && context.getRenderer();
+	const parent = renderer && renderer.getParent && renderer.getParent();
 	const parentName = parent ? getFunctionName(parent.constructor) : null;
 	const location = parentName ? `Check render method of '${parentName}'.` : '';
 	return new Error(
@@ -179,11 +190,7 @@ function composeError(error, name, context) {
  * @return {string} Type of value.
  */
 function getType(value) {
-	const type = typeof value;
-	if (Array.isArray(value)) {
-		return 'array';
-	}
-	return type;
+	return Array.isArray(value) ? 'array' : typeof value;
 }
 
 /**
@@ -196,49 +203,51 @@ function isInvalid(result) {
 }
 
 /**
- * Creates a validator that checks a value against a single type, null, or
+ * Wraps the given validator so that it also accepts null/undefined values.
+ *   a validator that checks a value against a single type, null, or
  * undefined.
- * @param {function()} typeValidator Validator to check value against.
- * @return {function()} Validator.
+ * @param {!function()} typeValidator Validator to wrap.
+ * @return {!function()} Wrapped validator.
  */
 function maybe(typeValidator) {
 	return (value, name, context) => {
-		if (!isDef(value) || isNull(value)) {
-			return true;
-		}
-		return typeValidator(value, name, context);
+		return isDefAndNotNull(value) ? typeValidator(value, name, context) : true;
 	};
 }
 
+
 /**
- * Creates a validator that checks against a specific primitive type. If this
- * validator is called with no arguments, it will return the actual validator
- * function instead of running it. That's done to allow all validators to be
- * used consistently, since some (like `arrayOf`) always require that you call
- * the function before receiving the actual validator.
- * @param {string} expectedType Type to check against.
- * @return {function()} Validator if called with arguments, or wrapper function
- *     that returns the validator otherwise.
+ * Checks if all the items of the given array pass the given validator.
+ * @param {!function()} validator
+ * @param {*} value The array to validate items for.
+ * @param {string} name The name of the array property being checked.
+ * @param {!Object} context Owner of the array property being checked.
+ * @return {!Error|boolean} `true` if the type matches, or an error otherwise.
  */
-function validateType(expectedType) {
-	const validatorFn = maybe((value, name, context) => {
-		const type = getType(value);
-		if (type !== expectedType) {
-			return composeError(
-				`Expected type '${expectedType}', but received type '${type}'.`,
-				name,
-				context
-			);
+function validateArrayItems(validator, value, name, context) {
+	for (let i = 0; i < value.length; i++) {
+		if (isInvalid(validator(value[i], name, context))) {
+			return composeError(ERROR_ARRAY_OF_TYPE, name, context);
 		}
-		return true;
-	});
-	return (...args) => {
-		if (args.length === 0) {
-			return validatorFn;
-		} else {
-			return validatorFn(...args);
-		}
-	};
+	}
+	return true;
+}
+
+/**
+ * Checks if the given value matches the expected type.
+ * @param {string} expectedType String representing the expected type.
+ * @param {*} value The value to match the type of.
+ * @param {string} name The name of the property being checked.
+ * @param {!Object} context Owner of the property being checked.
+ * @return {!Error|boolean} `true` if the type matches, or an error otherwise.
+ */
+function validateType(expectedType, value, name, context) {
+	const type = getType(value);
+	if (type !== expectedType) {
+		const msg = `Expected type '${expectedType}', but received type '${type}'.`;
+		return composeError(msg, name, context);
+	}
+	return true;
 }
 
 export default validators;

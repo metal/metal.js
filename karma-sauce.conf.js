9be2feae89e37c163f2cd9f55073227064ced292
Julien C <julien.castelain@liferay.com>
2017-04-18 11:19:29 +0200

    Adds custom-elements polyfill for tests

--- karma-sauce.conf.js
+++ karma-sauce.conf.js
@@ -10,6 +10,12 @@ module.exports = function(config) {
 			// Since all files will be added, we need to ensure manually that these
 			// will be added first.
 			{
+				pattern: 'packages/metal-custom-element/node_modules/@webcomponents/custom-elements/src/custom-elements.js',
+				watched: false,
+				included: true,
+				served: true
+			},
+			{
 				pattern: 'packages/metal-custom-element/node_modules/@webcomponents/custom-elements/src/native-shim.js',
 				watched: false,
 				included: true,
@@ -54,6 +60,7 @@ module.exports = function(config) {
 		],
 
 		preprocessors: {
+			'packages/metal-custom-element/node_modules/@webcomponents/custom-elements/src/custom-elements.js': ['browserify'],
 			'packages/metal-custom-element/node_modules/@webcomponents/custom-elements/src/native-shim.js': ['browserify'],
 			'packages/metal-incremental-dom/src/incremental-dom.js': ['browserify'],
 			'packages/metal-incremental-dom/lib/incremental-dom.js': ['browserify'],

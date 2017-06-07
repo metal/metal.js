4fbbbea51aa462adab6da67cbb6c008a925b6dab
Zeno Rocha <zno.rocha@gmail.com>
2017-06-07 10:59:58 -0700

    Generates Electric base site

--- packages/metaljs.com/gulpfile.js
+++ packages/metaljs.com/gulpfile.js
@@ -1,80 +1,18 @@
 'use strict';
 
-var gulp = require('gulp');
-var compass = require('gulp-compass');
-var connect = require('gulp-connect');
-var ghpages = require('gulp-gh-pages');
-var soynode = require('gulp-soynode');
+const electric = require('electric');
+const ghPages = require('gulp-gh-pages');
+const gulp = require('gulp');
 
-gulp.task('connect', function() {
-	connect.server({
-		root: 'dist/public'
-	});
+electric.registerTasks({
+	gulp: gulp
 });
 
-gulp.task('cname', function() {
-	return gulp.src('src/public/CNAME')
-		.pipe(gulp.dest('dist/public'));
-});
+// Deploy ----------------------------------------------------------------------
 
-gulp.task('container', function() {
-	return gulp.src('src/public/container.json')
-		.pipe(gulp.dest('dist/public'));
-});
-
-gulp.task('deploy', ['cname', 'container', 'build'], function() {
-	return gulp.src('dist/public/**/*')
-		.pipe(ghpages({
+gulp.task('deploy', ['build'], () => {
+	return gulp.src('dist/**/*')
+		.pipe(ghPages({
 			branch: 'wedeploy'
 		}));
 });
-
-gulp.task('downloads', function() {
-	return gulp.src('src/public/downloads/**')
-		.pipe(gulp.dest('dist/public/downloads'));
-});
-
-gulp.task('images', function() {
-	return gulp.src('src/public/images/**')
-		.pipe(gulp.dest('dist/public/images'));
-});
-
-gulp.task('scripts', function() {
-	return gulp.src('src/public/scripts/**')
-		.pipe(gulp.dest('dist/public/scripts'));
-});
-
-gulp.task('styles', function() {
-	return gulp.src('src/public/styles/*.scss')
-		.pipe(compass({
-			css: 'dist/public/styles',
-			sass: 'src/public/styles',
-			image: 'dist/public/images'
-		}))
-		.pipe(gulp.dest('dist/public/styles'));
-});
-
-gulp.task('soy', function() {
-	return gulp.src('src/**/*.soy')
-		.pipe(soynode({
-			renderSoyWeb: true
-		}))
-		.pipe(gulp.dest('dist'));
-});
-
-gulp.task('vendor', function() {
-	return gulp.src('src/public/vendor/**')
-		.pipe(gulp.dest('dist/public/vendor'));
-});
-
-gulp.task('watch', function () {
-	gulp.watch('src/public/downloads/**', ['downloads']);
-	gulp.watch('src/public/images/**', ['images']);
-	gulp.watch('src/public/scripts/**', ['scripts']);
-	gulp.watch('src/public/vendor/**', ['vendor']);
-	gulp.watch('src/public/styles/*.scss', ['styles']);
-	gulp.watch('src/**/*.soy', ['soy']);
-});
-
-gulp.task('build', ['images', 'downloads', 'scripts', 'vendor', 'soy', 'styles']);
-gulp.task('default', ['build', 'connect', 'watch']);

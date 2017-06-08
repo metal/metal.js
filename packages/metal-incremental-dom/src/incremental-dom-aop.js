a389b73f5f560d9786a32ca4e1004962c176dc95
Chema Balsas <jose.balsas@liferay.com>
2017-06-08 13:35:50 -0700

    Consolidates incremental-dom usage in just one module

--- packages/metal-incremental-dom/src/incremental-dom-aop.js
+++ packages/metal-incremental-dom/src/incremental-dom-aop.js
@@ -1,7 +1,6 @@
 'use strict';
 
 import './incremental-dom';
-import './incremental-dom-string';
 
 /**
  * Gets the original incremental dom functions.
--- packages/metal-incremental-dom/src/incremental-dom.js
+++ packages/metal-incremental-dom/src/incremental-dom.js
@@ -1,5 +1,19 @@
 import * as IncrementalDOM from 'incremental-dom';
+import * as IncrementalDOMString from 'incremental-dom-string';
 
-const scope = (typeof exports !== 'undefined' && typeof global !== 'undefined') ? global : window;
+// Sets to true if running inside Node.js environment with extra check for
+// `process.browser` to skip Karma runner environment. Karma environment has
+// `process` defined even though it runs on the browser.
+var isNode = (typeof process !== 'undefined') && !process.browser;
 
-scope.IncrementalDOM = scope.IncrementalDOM || IncrementalDOM;
+if (isNode && process.env.NODE_ENV !== 'test') {
+	// Overrides global.IncrementalDOM virtual elements with incremental dom
+	// string implementation for server side rendering. At the moment it does not
+	// override for Node.js tests since tests are using jsdom to simulate the
+	// browser.
+	global.IncrementalDOM = IncrementalDOMString;
+} else {
+	var scope = (typeof exports !== 'undefined' && typeof global !== 'undefined') ? global : window;
+
+	scope.IncrementalDOM = IncrementalDOM;
+}

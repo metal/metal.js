ce93d0a1e5d2a72944850a3473879b3c62ea9c9d
Chema Balsas <jose.balsas@liferay.com>
2017-06-08 17:16:09 -0700

    Adds isServerSide test for server-side rendering

--- packages/metal-incremental-dom/src/incremental-dom.js
+++ packages/metal-incremental-dom/src/incremental-dom.js
@@ -1,12 +1,8 @@
 import * as IncrementalDOM from 'incremental-dom';
 import * as IncrementalDOMString from 'incremental-dom-string';
+import { isServerSide } from 'metal';
 
-// Sets to true if running inside Node.js environment with extra check for
-// `process.browser` to skip Karma runner environment. Karma environment has
-// `process` defined even though it runs on the browser.
-var isNode = (typeof process !== 'undefined') && !process.browser;
-
-if (isNode && process.env.NODE_ENV !== 'test') {
+if (isServerSide()) {
 	// Overrides global.IncrementalDOM virtual elements with incremental dom
 	// string implementation for server side rendering. At the moment it does not
 	// override for Node.js tests since tests are using jsdom to simulate the

956a8fdc7e87a2157cff5607e4877f60f8ba9f04
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2017-09-27 16:04:00 -0700

    Don't use preprocessor for polyfill

--- karma-sauce.conf.js
+++ karma-sauce.conf.js
@@ -70,6 +70,7 @@ module.exports = function(config) {
 			'packages/metal-incremental-dom/lib/incremental-dom.js': ['browserify'],
 			'packages/metal-soy/node_modules/metal-soy-bundle/lib/bundle.js': ['browserify'],
 			'packages/metal-soy/node_modules/html2incdom/lib/*.js': ['browserify'],
+			'packages/metal-custom-element/node_modules/@webcomponents/webcomponentsjs/webcomponents-lite.js': [],
 			'packages/metal*/test/**/*.js': ['browserify']
 		},
 

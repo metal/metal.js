4c2a707bf2f2fcb24588dc799fdbfcf09fc1f817
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2017-10-11 09:30:43 -0700

    Don't access client side globals if rendering server side

--- packages/metal-component/src/Component.js
+++ packages/metal-component/src/Component.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import { addListenersFromObj } from './events/events';
-import { getStaticProperty, isBoolean, isDefAndNotNull, isElement, isObject, isString, object } from 'metal';
+import { getStaticProperty, isBoolean, isDefAndNotNull, isElement, isObject, isServerSide, isString, object } from 'metal';
 import { syncState } from './sync/sync';
 import { DomEventEmitterProxy, toElement } from 'metal-dom';
 import ComponentDataManager from './ComponentDataManager';
@@ -98,6 +98,13 @@ class Component extends EventEmitter {
 		this.initialConfig_ = opt_config || {};
 
 		/**
+		 * Whether the current environment is server side.
+		 * @type {boolean}
+		 * @protected
+		 */
+		this.serverSide_ = isServerSide();
+
+		/**
 		 * Whether the element was rendered.
 		 * @type {boolean}
 		 */
@@ -109,7 +116,7 @@ class Component extends EventEmitter {
 		 * `attach`.
 		 * @type {!Element}
 		 */
-		this.DEFAULT_ELEMENT_PARENT = document.body;
+		this.DEFAULT_ELEMENT_PARENT = !this.serverSide_ ? document.body : null;
 
 		this.setShouldUseFacade(true);
 		this.element = this.initialConfig_.element;
@@ -495,7 +502,7 @@ class Component extends EventEmitter {
 	 */
 	renderComponent(opt_parentElement) {
 		if (!this.hasRendererRendered_) {
-			if (window.__METAL_DEV_TOOLS_HOOK__) {
+			if (!this.serverSide_ && window.__METAL_DEV_TOOLS_HOOK__) {
 				window.__METAL_DEV_TOOLS_HOOK__(this);
 			}
 			this.getRenderer().render(this);

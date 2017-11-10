61d0f4fc1cb5ac4b4c5c90e891892bcc1e49bea8
Chema Balsas <jose.balsas@liferay.com>
2017-11-10 15:27:19 -0800

    Uses var since polyfill does not get transpiled

--- packages/metal-web-component/webcomponents_polyfill.js
+++ packages/metal-web-component/webcomponents_polyfill.js
@@ -1,8 +1,10 @@
+/* eslint no-var: 0 */
 if (window.navigator.userAgent.indexOf('MSIE') === -1) {
-	const script = document.createElement('script');
+	var script = document.createElement('script');
 
 	script.crossOrigin = 'anonymous';
-	script.src = '/base/packages/metal-web-component/node_modules/@webcomponents/webcomponentsjs/webcomponents-lite.js';
+	script.src =
+		'/base/packages/metal-web-component/node_modules/@webcomponents/webcomponentsjs/webcomponents-lite.js';
 	script.type = 'text/javascript';
 
 	document.body.appendChild(script);

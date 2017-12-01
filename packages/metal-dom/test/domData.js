b7a89626ef5d89d5dc710485ec91cc8059f1fc43
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2017-12-01 09:15:51 -0800

    Do not modify pre-existing tests for get method

--- packages/metal-dom/test/domData.js
+++ packages/metal-dom/test/domData.js
@@ -3,9 +3,20 @@
 import domData from '../src/domData';
 
 describe('domData', function() {
-	it('should set data object from element', function() {
-		let data = domData.set(document.createElement('div'));
+	it('should get data object from element', function() {
+		let data = domData.get(document.createElement('div'));
 		assert.ok(data);
+	});
+
+	it('should return same data object for the same element', function() {
+		let element = document.createElement('div');
+		assert.strictEqual(domData.get(element), domData.get(element));
+	});
+
+	it('should return different data objects for the different elements', function() {
+		let element1 = document.createElement('div');
+		let element2 = document.createElement('div');
+		assert.notStrictEqual(domData.get(element1), domData.get(element2));
 	});
 
 	it('should get data object from element', function() {
@@ -35,16 +46,5 @@ describe('domData', function() {
 		domData.set(element, 'value', 20);
 		let data = domData.get(element);
 		assert.strictEqual(data.value, 20);
-	});
-
-	it('should return same data object for the same element', function() {
-		let element = document.createElement('div');
-		assert.strictEqual(domData.get(element), domData.get(element));
-	});
-
-	it('should return different data objects for the different elements', function() {
-		let element1 = document.createElement('div');
-		let element2 = document.createElement('div');
-		assert.notStrictEqual(domData.set(element1), domData.set(element2));
 	});
 });

bcd29d297e50c59d07f555c12529a4b450e5593d
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2017-12-27 12:53:27 -0800

    Adds Fragment component

--- packages/metal-jsx/src/all/jsx.js
+++ packages/metal-jsx/src/all/jsx.js
@@ -3,6 +3,7 @@
 import {validators, Config} from 'metal-state';
 import DangerouslySetHTML from '../DangerouslySetHTML';
 import JSXComponent from '../JSXComponent';
+import Fragment from '../Fragment';
 
 export default JSXComponent;
-export {DangerouslySetHTML, validators, Config, JSXComponent};
+export {DangerouslySetHTML, validators, Config, JSXComponent, Fragment};

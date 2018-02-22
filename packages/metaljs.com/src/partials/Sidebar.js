72ae6f53bf5bc825b77bf481fa9e4dabc16d5215
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2018-02-22 11:41:25 -0800

    Update metaljs.com for use with Electric v3.0.0

--- packages/metaljs.com/src/partials/Sidebar.js
+++ packages/metaljs.com/src/partials/Sidebar.js
@@ -4,7 +4,7 @@ import Component from 'metal-component';
 import Soy from 'metal-soy';
 import Toggler from 'metal-toggler';
 
-import templates from './Sidebar.soy';
+import templates from './Sidebar.soy.js';
 
 class Sidebar extends Component {
 	attached() {

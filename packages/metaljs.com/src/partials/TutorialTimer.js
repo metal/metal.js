c46987b2365c79ca5f3cc34262b33595533d19ba
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2018-02-22 11:41:25 -0800

    Update metaljs.com for use with Electric v3.0.0 | Fixes #360

--- packages/metaljs.com/src/partials/TutorialTimer.js
+++ packages/metaljs.com/src/partials/TutorialTimer.js
@@ -4,11 +4,16 @@ import Component from 'metal-component';
 import Soy from 'metal-soy';
 import moment from 'moment';
 import {addClasses, hasClass} from 'metal-dom';
+import {isServerSide} from 'metal';
 
-import templates from './TutorialTimer.soy';
+import templates from './TutorialTimer.soy.js';
 
 class TutorialTimer extends Component {
 	attached() {
+		if (isServerSide()) {
+			return;
+		}
+
 		this.time = this.calculateTimeRemaining();
 	}
 

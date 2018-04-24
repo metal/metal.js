5cf93bc7dce258fc6ab16355ccbcf0263f83c1df
Matuzalém S. Teles <matuzalemteles@gmail.com>
2018-04-24 16:27:06 -0300

    Move the `a` tag from the block to inline in HTMLParser | Fixes #371

--- packages/metal-incremental-dom/src/html/HTMLParser.js
+++ packages/metal-incremental-dom/src/html/HTMLParser.js
@@ -97,12 +97,12 @@ var empty = makeMap(
 
 // Block Elements - HTML 5
 var block = makeMap(
-	'a,address,article,applet,aside,audio,blockquote,button,canvas,center,dd,del,dir,div,dl,dt,fieldset,figcaption,figure,footer,form,frameset,h1,h2,h3,h4,h5,h6,header,hgroup,hr,iframe,ins,isindex,li,map,menu,noframes,noscript,object,ol,output,p,pre,section,script,table,tbody,td,tfoot,th,thead,tr,ul,video'
+	'address,article,applet,aside,audio,blockquote,button,canvas,center,dd,del,dir,div,dl,dt,fieldset,figcaption,figure,footer,form,frameset,h1,h2,h3,h4,h5,h6,header,hgroup,hr,iframe,ins,isindex,li,map,menu,noframes,noscript,object,ol,output,p,pre,section,script,table,tbody,td,tfoot,th,thead,tr,ul,video'
 );
 
 // Inline Elements - HTML 5
 var inline = makeMap(
-	'abbr,acronym,applet,b,basefont,bdo,big,br,button,cite,code,del,dfn,em,font,i,iframe,img,input,ins,kbd,label,map,object,q,s,samp,script,select,small,span,strike,strong,sub,sup,textarea,tt,u,var'
+	'a,abbr,acronym,applet,b,basefont,bdo,big,br,button,cite,code,del,dfn,em,font,i,iframe,img,input,ins,kbd,label,map,object,q,s,samp,script,select,small,span,strike,strong,sub,sup,textarea,tt,u,var'
 );
 
 // Elements that you can, intentionally, leave open
